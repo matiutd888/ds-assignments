f0c84c692adfe763ab05d3b6a2b7fd4de1ad6e1f
Mateusz <matiutd888@gmail.com>
2022-12-09 05:01:54 +0100

    Extend main.rs test

--- dslab09/main.rs
+++ dslab09/main.rs
@@ -26,7 +26,9 @@ async fn main() {
     let mut system = System::new().await;
     let (transaction_done_tx, transaction_done_rx) = unbounded();
     let laptop_id = Uuid::new_v4();
+    let book_id = Uuid::new_v4();
     let initial_laptop_price = 150000;
+    let book_price = 5000;
     let products = vec![
         Product {
             identifier: laptop_id,
@@ -34,9 +36,9 @@ async fn main() {
             price: initial_laptop_price,
         },
         Product {
-            identifier: Uuid::new_v4(),
+            identifier: book_id,
             pr_type: ProductType::Books,
-            price: 5000,
+            price: book_price,
         },
         Product {
             identifier: Uuid::new_v4(),
@@ -51,6 +53,7 @@ async fn main() {
 
     // Increase prices of electronics:
     let electronics_price_shift = 100;
+    let tx2 = transaction_done_tx.clone();
     cyber_store
         .send(TransactionMessage {
             transaction: Transaction {
@@ -59,19 +62,36 @@ async fn main() {
             },
             completed_callback: Box::new(|result| {
                 Box::pin(async move {
-                    transaction_done_tx.send(result).await.unwrap();
+                    tx2.send(result).await.unwrap();
                 })
             }),
         })
         .await;
     assert_eq!(Ok(TwoPhaseResult::Ok), transaction_done_rx.recv().await);
 
+    let tx3 = transaction_done_tx.clone();
+    cyber_store
+        .send(TransactionMessage {
+            transaction: Transaction {
+                pr_type: ProductType::Books,
+                shift: -(book_price as i32),
+            },
+            completed_callback: Box::new(|result| {
+                Box::pin(async move {
+                    tx3.send(result).await.unwrap();
+                })
+            }),
+        })
+        .await;
+
+    assert_eq!(Ok(TwoPhaseResult::Abort), transaction_done_rx.recv().await);
     // Check the new price of the latop:
     tokio::time::sleep(Duration::from_millis(100)).await;
     assert_eq!(
         initial_laptop_price + (electronics_price_shift as u64),
         send_query(&node, laptop_id).await.0.unwrap()
     );
+    assert_eq!(book_price, send_query(&node, book_id).await.0.unwrap());
 
     println!("System can execute a simple transaction!");
     system.shutdown().await;
