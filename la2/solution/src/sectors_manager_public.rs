816c612b1fdf02f1c9193791ae14056fbe50c7bf
Mateusz <matiutd888@gmail.com>
2022-12-17 03:07:08 +0100

    Fix Sectors manager

--- la2/solution/src/sectors_manager_public.rs
+++ la2/solution/src/sectors_manager_public.rs
@@ -1,9 +1,10 @@
-use hmac::digest::typenum::Length;
 use core::time;
+use hmac::digest::typenum::Length;
 use std::collections::HashMap;
 use std::ffi::OsString;
 use tokio::fs::{self, rename, File};
 use tokio::io::AsyncWriteExt;
+use tokio::sync::RwLock;
 
 use crate::{constants, SectorIdx, SectorVec};
 use std::path::PathBuf;
@@ -27,28 +28,31 @@ pub trait SectorsManager: Send + Sync {
 /// Path parameter points to a directory to which this method has exclusive access.
 pub fn build_sectors_manager(path: PathBuf) -> Arc<dyn SectorsManager> {
     let mut metadata_map = HashMap::new();
-    
-    let paths = std::fs::read_dir(path).unwrap();
 
-    for path in paths {
-        println!("Name: {}", path.unwrap().path().display());
-        if let Some(filename) = path.unwrap().path().file_name() {
+    let paths = std::fs::read_dir(&path).unwrap();
+
+    for path_it in paths {
+        if let Some(filename) = path_it.unwrap().path().file_name() {
             if let Some(string) = filename.to_str() {
-                let mut split = string.split(&SectorsManagerImpl::delimeter);
+                let split = string.split("-");
                 let v = split.collect::<Vec<&str>>();
                 if v.len() == 3 {
                     let idx = v.get(0).unwrap().parse::<u64>();
                     let timestamp = v.get(1).unwrap().parse::<u64>();
                     let write_rank = v.get(2).unwrap().parse::<u8>();
                     if idx.is_ok() && timestamp.is_ok() && write_rank.is_ok() {
-                        assert!(metadata_map.insert(idx.unwrap(), (timestamp.unwrap(), write_rank.unwrap())) == None);
+                        assert!(
+                            metadata_map
+                                .insert(idx.unwrap(), (timestamp.unwrap(), write_rank.unwrap()))
+                                == None
+                        );
                     }
                 }
             }
         }
     }
     Arc::new(SectorsManagerImpl {
-        sectors_metadata: metadata_map,
+        sectors_metadata: RwLock::new(metadata_map),
         root_storage_dir: path,
     })
 }
@@ -56,13 +60,11 @@ pub fn build_sectors_manager(path: PathBuf) -> Arc<dyn SectorsManager> {
 type Metadata = (u64, u8);
 
 struct SectorsManagerImpl {
-    sectors_metadata: HashMap<SectorIdx, Metadata>,
+    sectors_metadata: RwLock<HashMap<SectorIdx, Metadata>>,
     root_storage_dir: PathBuf,
 }
 
 impl SectorsManagerImpl {
-    const delimeter: String = String::from("-");
-    const ZERO_SECTOR: Vec<u8> = vec![0; constants::SECTOR_SIZE_BYTES];
     const DEFAULT_METADATA: Metadata = (0, 0);
 
     fn create_filename(idx: &SectorIdx, metadata: &Metadata) -> String {
@@ -85,14 +87,14 @@ impl SectorsManagerImpl {
 impl SectorsManager for SectorsManagerImpl {
     /// Returns 4096 bytes of sector data by index.
     async fn read_data(&self, idx: SectorIdx) -> SectorVec {
-        let result_vec = if let Some(metadata) = self.sectors_metadata.get(&idx) {
+        let result_vec = if let Some(metadata) = self.sectors_metadata.read().await.get(&idx) {
             let file_to_read = Self::create_filename(&idx, metadata);
             let file_path = self.append_to_path(file_to_read);
             let result = fs::read(file_path).await.unwrap();
             result
         } else {
             // If a sector was never written, we assume that both the logical timestamp and the write rank are 0, and that it contains 4096 zero bytes.
-            Self::ZERO_SECTOR.clone()
+            vec![0; constants::SECTOR_SIZE_BYTES]
         };
         assert!(result_vec.len() == constants::SECTOR_SIZE_BYTES);
 
@@ -103,7 +105,7 @@ impl SectorsManager for SectorsManagerImpl {
     /// Timestamps and ranks are relevant for atomic register algorithm, and are described
     /// there.
     async fn read_metadata(&self, idx: SectorIdx) -> (u64, u8) {
-        if let Some(metadata) = self.sectors_metadata.get(&idx) {
+        if let Some(metadata) = self.sectors_metadata.read().await.get(&idx) {
             metadata.clone()
         } else {
             Self::DEFAULT_METADATA.clone()
@@ -124,17 +126,20 @@ impl SectorsManager for SectorsManagerImpl {
         tmp_file.write_all(&(sector.0 .0)).await.unwrap();
         tmp_file.sync_data().await.unwrap();
 
-        if let Some(metadata) = self.sectors_metadata.get(&idx) {
-            let current_path = self.append_to_path(Self::create_filename(&idx, metadata));
-            fs::remove_file(key_path).await.unwrap();
-            directory.sync_data().await.unwrap();
-        }
-
         rename(tmp_path, key_path).await.unwrap();
         directory.sync_data().await.unwrap();
 
-        self.sectors_metadata
-            .insert(idx, (sector.1, sector.2))
-            .unwrap();
+        let should_remove_file = self.sectors_metadata.read().await.get(&idx).cloned();
+
+        {
+            let mut w = self.sectors_metadata.write().await;
+            (*w).insert(idx, (sector.1, sector.2));
+        }
+
+        if let Some(metadata) = should_remove_file {
+            let path_to_remove = self.append_to_path(Self::create_filename(&idx, &metadata));
+            fs::remove_file(path_to_remove).await.unwrap();
+            directory.sync_data().await.unwrap();
+        }
     }
 }
