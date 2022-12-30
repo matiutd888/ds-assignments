a64a42d9291d1811d37927e93a92cdeb093404aa
Mateusz <matiutd888@gmail.com>
2022-12-30 06:03:34 +0100

    Comment out register client

--- la2/solution/src/register_client_public.rs
+++ la2/solution/src/register_client_public.rs
@@ -35,220 +35,220 @@ pub struct Send {
     pub target: u8,
 }
 
-pub struct RegisterClientImpl {
-    to_rebroadcast: Arc<RwLock<HashMap<SectorIdx, Broadcast>>>,
-    self_sender: Arc<dyn MySender<SystemRegisterCommand>>,
-    tcp_sender: Sender<Send>,
-    self_rank: u8,
-    processes_count: usize,
-}
+// pub struct RegisterClientImpl {
+//     to_rebroadcast: Arc<RwLock<HashMap<SectorIdx, Broadcast>>>,
+//     self_sender: Arc<dyn MySender<SystemRegisterCommand>>,
+//     tcp_sender: Sender<Send>,
+//     self_rank: u8,
+//     processes_count: usize,
+// }
 
-impl RegisterClientImpl {
-    const CHANNEL_SIZE: usize = 5000;
+// impl RegisterClientImpl {
+//     const CHANNEL_SIZE: usize = 5000;
 
-    pub async fn new(
-        self_rank: u8,
-        processes_count: usize,
-        tcp_locations: Vec<(String, u16)>,
-        hmac_key: [u8; 64],
-        self_sender: Arc<dyn MySender<SystemRegisterCommand>>,
-    ) -> RegisterClientImpl {
-        let (s, r) = channel(Self::CHANNEL_SIZE);
-        let mut tcp_sender = TcpSender::new(tcp_locations, r, hmac_key);
-        tokio::spawn(async move {
-            tcp_sender.send_in_loop().await;
-        });
-        let register_client = RegisterClientImpl {
-            to_rebroadcast: Arc::new(RwLock::new(HashMap::new())),
-            self_sender: self_sender,
-            tcp_sender: s,
-            self_rank: self_rank,
-            processes_count: processes_count,
-        };
-        register_client
-            .spawn_timer(Duration::from_millis(2000))
-            .await;
-        register_client
-    }
+//     pub async fn new(
+//         self_rank: u8,
+//         processes_count: usize,
+//         tcp_locations: Vec<(String, u16)>,
+//         hmac_key: [u8; 64],
+//         self_sender: Arc<dyn MySender<SystemRegisterCommand>>,
+//     ) -> RegisterClientImpl {
+//         let (s, r) = channel(Self::CHANNEL_SIZE);
+//         let mut tcp_sender = TcpSender::new(tcp_locations, r, hmac_key);
+//         tokio::spawn(async move {
+//             tcp_sender.send_in_loop().await;
+//         });
+//         let register_client = RegisterClientImpl {
+//             to_rebroadcast: Arc::new(RwLock::new(HashMap::new())),
+//             self_sender: self_sender,
+//             tcp_sender: s,
+//             self_rank: self_rank,
+//             processes_count: processes_count,
+//         };
+//         register_client
+//             .spawn_timer(Duration::from_millis(2000))
+//             .await;
+//         register_client
+//     }
 
-    async fn spawn_timer(&self, duration: Duration) {
-        let to_rebroadcast = self.to_rebroadcast.clone();
-        let mut interval = time::interval(duration);
+//     async fn spawn_timer(&self, duration: Duration) {
+//         let to_rebroadcast = self.to_rebroadcast.clone();
+//         let mut interval = time::interval(duration);
 
-        let tcp_sender = self.tcp_sender.clone();
-        let self_sender = self.self_sender.clone();
+//         let tcp_sender = self.tcp_sender.clone();
+//         let self_sender = self.self_sender.clone();
 
-        let processes_count = self.processes_count;
-        let self_rank = self.self_rank;
+//         let processes_count = self.processes_count;
+//         let self_rank = self.self_rank;
 
-        tokio::spawn(async move {
-            loop {
-                interval.tick().await;
-                let guard = to_rebroadcast.read().await;
-                for (_, b) in guard.iter() {
-                    Self::handle_broadcast(
-                        b,
-                        &self_sender,
-                        &tcp_sender,
-                        processes_count,
-                        self_rank,
-                    )
-                    .await;
-                }
-            }
-        });
-    }
+//         tokio::spawn(async move {
+//             loop {
+//                 interval.tick().await;
+//                 let guard = to_rebroadcast.read().await;
+//                 for (_, b) in guard.iter() {
+//                     Self::handle_broadcast(
+//                         b,
+//                         &self_sender,
+//                         &tcp_sender,
+//                         processes_count,
+//                         self_rank,
+//                     )
+//                     .await;
+//                 }
+//             }
+//         });
+//     }
 
-    async fn handle_broadcast(
-        b: &Broadcast,
-        self_sender: &Arc<dyn MySender<SystemRegisterCommand>>,
-        tcp_sender: &Sender<Send>,
-        processes_count: usize,
-        self_rank: u8,
-    ) {
-        for target in 1..=processes_count {
-            let target_u8 = target as u8;
-            if target_u8 == self_rank {
-                self_sender.send(b.cmd.as_ref().clone()).await;
-            } else {
-                tcp_sender
-                    .send(Send {
-                        cmd: b.cmd.clone(),
-                        target: target_u8,
-                    })
-                    .await
-                    .unwrap();
-            }
-        }
-    }
+//     async fn handle_broadcast(
+//         b: &Broadcast,
+//         self_sender: &Arc<dyn MySender<SystemRegisterCommand>>,
+//         tcp_sender: &Sender<Send>,
+//         processes_count: usize,
+//         self_rank: u8,
+//     ) {
+//         for target in 1..=processes_count {
+//             let target_u8 = target as u8;
+//             if target_u8 == self_rank {
+//                 self_sender.send(b.cmd.as_ref().clone()).await;
+//             } else {
+//                 tcp_sender
+//                     .send(Send {
+//                         cmd: b.cmd.clone(),
+//                         target: target_u8,
+//                     })
+//                     .await
+//                     .unwrap();
+//             }
+//         }
+//     }
 
-    pub async fn cancel_broadcast(&self, sector_idx: SectorIdx) {
-        self.to_rebroadcast.write().await.remove(&sector_idx);
-    }
+//     pub async fn cancel_broadcast(&self, sector_idx: SectorIdx) {
+//         self.to_rebroadcast.write().await.remove(&sector_idx);
+//     }
 
-    pub async fn insert_broadcast(&self, sector_idx: SectorIdx, broadcast: Broadcast) {
-        self.to_rebroadcast
-            .write()
-            .await
-            .insert(sector_idx, broadcast);
-    }
-}
+//     pub async fn insert_broadcast(&self, sector_idx: SectorIdx, broadcast: Broadcast) {
+//         self.to_rebroadcast
+//             .write()
+//             .await
+//             .insert(sector_idx, broadcast);
+//     }
+// }
 
-struct TcpSender {
-    tcp_locations: Vec<(String, u16)>,
-    streams: Vec<Option<TcpStream>>,
-    receiver: Receiver<Send>,
-    hmac_key: [u8; 64],
-}
+// struct TcpSender {
+//     tcp_locations: Vec<(String, u16)>,
+//     streams: Vec<Option<TcpStream>>,
+//     receiver: Receiver<Send>,
+//     hmac_key: [u8; 64],
+// }
 
-impl TcpSender {
-    pub fn new(
-        tcp_locations: Vec<(String, u16)>,
-        receiver: Receiver<Send>,
-        hmac_key: [u8; 64],
-    ) -> TcpSender {
-        let mut streams = Vec::new();
-        for _ in 0..tcp_locations.len() {
-            streams.push(None);
-        }
+// impl TcpSender {
+//     pub fn new(
+//         tcp_locations: Vec<(String, u16)>,
+//         receiver: Receiver<Send>,
+//         hmac_key: [u8; 64],
+//     ) -> TcpSender {
+//         let mut streams = Vec::new();
+//         for _ in 0..tcp_locations.len() {
+//             streams.push(None);
+//         }
 
-        TcpSender {
-            tcp_locations: tcp_locations,
-            streams: streams,
-            receiver: receiver,
-            hmac_key: hmac_key,
-        }
-    }
+//         TcpSender {
+//             tcp_locations: tcp_locations,
+//             streams: streams,
+//             receiver: receiver,
+//             hmac_key: hmac_key,
+//         }
+//     }
 
-    async fn send_in_loop(&mut self) {
-        loop {
-            let prev: Instant = Instant::now();
-            let new_command = self.receiver.recv().await.unwrap();
-            println!("Waited {:?}", Instant::now().duration_since(prev));
-            let index = new_command.target as usize - 1;
+//     async fn send_in_loop(&mut self) {
+//         loop {
+//             let prev: Instant = Instant::now();
+//             let new_command = self.receiver.recv().await.unwrap();
+//             println!("Waited {:?}", Instant::now().duration_since(prev));
+//             let index = new_command.target as usize - 1;
 
-            log::debug!(
-                "Trying to send to process {} from address {:?}",
-                new_command.target,
-                self.tcp_locations.get(index).unwrap()
-            );
-            if self.streams.get(index).unwrap().is_none() {
-                println!(
-                    "No stream to process {}, trying to connect",
-                    new_command.target
-                );
-                self.connect(index).await;
-            }
+//             log::debug!(
+//                 "Trying to send to process {} from address {:?}",
+//                 new_command.target,
+//                 self.tcp_locations.get(index).unwrap()
+//             );
+//             if self.streams.get(index).unwrap().is_none() {
+//                 println!(
+//                     "No stream to process {}, trying to connect",
+//                     new_command.target
+//                 );
+//                 self.connect(index).await;
+//             }
 
-            let s = self.streams.get_mut(index).unwrap();
-            if let Some(tcp) = s {
-                match serialize_register_command(
-                    &RegisterCommand::System(new_command.cmd.as_ref().clone()),
-                    tcp,
-                    &self.hmac_key,
-                )
-                .await
-                {
-                    Ok(_) => {
-                        log::debug!("serialize to {} successful", new_command.target);
-                    }
-                    Err(_) => {
-                        log::debug!("serialize to {} not successful", new_command.target);
-                        *s = None;
-                    }
-                }
-            } else {
-                log::debug!("Couldnt connect to process {}", new_command.target);
-            }
-        }
-    }
+//             let s = self.streams.get_mut(index).unwrap();
+//             if let Some(tcp) = s {
+//                 match serialize_register_command(
+//                     &RegisterCommand::System(new_command.cmd.as_ref().clone()),
+//                     tcp,
+//                     &self.hmac_key,
+//                 )
+//                 .await
+//                 {
+//                     Ok(_) => {
+//                         log::debug!("serialize to {} successful", new_command.target);
+//                     }
+//                     Err(_) => {
+//                         log::debug!("serialize to {} not successful", new_command.target);
+//                         *s = None;
+//                     }
+//                 }
+//             } else {
+//                 log::debug!("Couldnt connect to process {}", new_command.target);
+//             }
+//         }
+//     }
 
-    async fn connect(&mut self, index: usize) {
-        log::debug!("Connecting to {:?}", self.tcp_locations.get(index).unwrap());
-        self.streams.insert(
-            index,
-            TcpStream::connect(self.tcp_locations.get(index).unwrap())
-                .await
-                .ok(),
-        )
-    }
-}
+//     async fn connect(&mut self, index: usize) {
+//         log::debug!("Connecting to {:?}", self.tcp_locations.get(index).unwrap());
+//         self.streams.insert(
+//             index,
+//             TcpStream::connect(self.tcp_locations.get(index).unwrap())
+//                 .await
+//                 .ok(),
+//         )
+//     }
+// }
 
-// For sure it implements stubborn link algorithm
-#[async_trait::async_trait]
-impl RegisterClient for RegisterClientImpl {
-    async fn send(&self, msg: Send) {
-        // log::debug!(
-        //     "{} received send message for process {}",
-        //     self.self_rank,
-        //     msg.target
-        // );
-        if self.self_rank == msg.target {
-            self.self_sender.send(msg.cmd.as_ref().clone()).await;
-        } else {
-            self.tcp_sender
-                .send(Send {
-                    cmd: msg.cmd.clone(),
-                    target: msg.target,
-                })
-                .await
-                .unwrap();
-        }
-    }
+// // For sure it implements stubborn link algorithm
+// #[async_trait::async_trait]
+// impl RegisterClient for RegisterClientImpl {
+//     async fn send(&self, msg: Send) {
+//         // log::debug!(
+//         //     "{} received send message for process {}",
+//         //     self.self_rank,
+//         //     msg.target
+//         // );
+//         if self.self_rank == msg.target {
+//             self.self_sender.send(msg.cmd.as_ref().clone()).await;
+//         } else {
+//             self.tcp_sender
+//                 .send(Send {
+//                     cmd: msg.cmd.clone(),
+//                     target: msg.target,
+//                 })
+//                 .await
+//                 .unwrap();
+//         }
+//     }
 
-    async fn broadcast(&self, msg: Broadcast) {
-        println!("Broadcast {}", self.processes_count);
-        Self::handle_broadcast(
-            &msg,
-            &self.self_sender,
-            &self.tcp_sender,
-            self.processes_count,
-            self.self_rank,
-        )
-        .await;
-        self.insert_broadcast(msg.cmd.header.sector_idx, msg).await;
-    }
-}
+//     async fn broadcast(&self, msg: Broadcast) {
+//         println!("Broadcast {}", self.processes_count);
+//         Self::handle_broadcast(
+//             &msg,
+//             &self.self_sender,
+//             &self.tcp_sender,
+//             self.processes_count,
+//             self.self_rank,
+//         )
+//         .await;
+//         self.insert_broadcast(msg.cmd.header.sector_idx, msg).await;
+//     }
+// }
 
 pub struct NewRegisterClientImpl {
     to_rebroadcast: Arc<RwLock<HashMap<SectorIdx, Broadcast>>>,
